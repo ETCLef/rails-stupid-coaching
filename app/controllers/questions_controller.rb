55cea2ace266ab3aa04c4ae457ca7101ad698c55
Emilien Lefevre <emilienlefevre93@gmail.com>
2018-06-11 14:41:19 +0200

    hello

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,27 @@
+class QuestionsController < ApplicationController
+
+
+  def ask
+     @question = params[:question]
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work right now!"
+      @answer = "Great !"
+    elsif @question.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+# if @message == "I am going to work right now!"
+#     return ""
+#   elsif @message.include? "?"
+#     return "Silly question, get dressed and go to work!"
+#   else
+#     return "I don't care, get dressed and go to work!"
+#   end
+
